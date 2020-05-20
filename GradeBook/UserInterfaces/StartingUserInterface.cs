340cfbc496b610d0ff124269ba3a06b62a2f2ef7
mMoukhtar <mohamed.moukhtar@outlook.com>
2020-05-21 00:19:19 +0200

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -51,7 +51,7 @@ namespace GradeBook.UserInterfaces
                 gradeBook.Type = Enums.GradeBookType.Ranked;
             } else
             {
-                Console.WriteLine($"{parts[2]}  is not a supported type of gradebook, please try again");
+                Console.WriteLine($"{parts[2]} is not a supported type of gradebook, please try again");
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
